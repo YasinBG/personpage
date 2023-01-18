ae71495a4dd56be8babcba17ea04342ae44960f1
Yasin BUYUKGUZEL <116117219+YasinBG@users.noreply.github.com>
2023-01-19 00:28:03 +0300

    cursor pointer eklendi

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="bg-indigo-300 text-gray-900 w-48 h-10 rounded-lg items-center text-center p-2 text-l font-semibold shadow-lg transition-all hover:bg-indigo-700 hover:text-white ">
+    <button class="bg-indigo-300 text-gray-900 w-48 h-10 rounded-lg items-center text-center p-2 text-l font-semibold shadow-lg transition-all cursor:pointer hover:bg-indigo-700 hover:text-white ">
         <slot name="button"></slot>
     </button>
   </div>
