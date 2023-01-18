8be33987076f7baf4dac8ee097bc9c4a58050b1f
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-01-18 21:41:17 +0200

    personpage

---
+++ src/Layout/CardText.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=" w-4/7 text-center mx-auto my-4 px-2">
+    
+    <h3 class="font-semibold text-2xl text-gray-800 text-center mx-auto ">
+        <slot name="title"></slot>
+    </h3>
+    <p class="text-center text-gray-600 mx-auto w-3/5">
+        <slot name="content"></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
