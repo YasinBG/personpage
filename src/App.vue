6cba50fc2482d176bd6e0a883fab467ba08919f7
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-01-18 22:20:46 +0200

    button

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,24 @@
     <CardText>
       <template #content> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorem optio omnis, hic ut consequuntur odit suscipit tempore reiciendis soluta quis sint dolore aliquid aperiam ullam eius harum. Voluptatibus, excepturi? </template>
     </CardText>
+
   </div>
+
+  <Button>
+    <template #button>BUTTON</template>
+  </Button>
 </template>
 
 <script>
+import Button from './components/Button.vue'
 import CardText from './Layout/CardText.vue'
+
 
 export default {
   name:"App",
   components:{
-    CardText
+    CardText,
+    Button
   },
 
 }
---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button class="bg-indigo-300 text-gray-900 w-48 h-10 rounded-lg items-center text-center p-2 text-l font-semibold shadow-lg transition-all hover:bg-indigo-700 hover:text-white ">
+        <slot name="button"></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
